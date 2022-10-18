318d4a6b767ba13a2cdc2c5332e3cc98aef5af8c
Mark Izraelson <mizraelson@gmail.com>
2022-10-18 16:39:48 -0600

    mixin + -> --

--- docs/mixcr/guides/generic-multiplex-bcr/scripts/020-upstream-example.sh
+++ docs/mixcr/guides/generic-multiplex-bcr/scripts/020-upstream-example.sh
@@ -1,10 +1,10 @@
 mixcr analyze bcr_amplicon \
-    +species mmu \
-    +rna \
-    +rigidLeftAlignmentBoundary \
-    +rigidRightAlignmentBoundary \
-    +tagPattern ^N{4}ccctcctttaattccc{21}(R1:*)\^N{4}gaggagagagagagagN{26}(R2:*) \
-    +assembleClonotypesBy {CDR1Begin:FR4End} \
+    --species mmu \
+    --rna \
+    --rigid-left-alignment-boundary \
+    --rigid-right-alignment-boundary \
+    --tagPattern ^N{4}ccctcctttaattccc{21}(R1:*)\^N{4}gaggagagagagagagN{26}(R2:*) \
+    --assemble-clonotypes-by {CDR1Begin:FR4End} \
     raw/M1_4T1_replica1_Blood_R1.fastq.gz \
     raw/M1_4T1_replica1_Blood_R2.fastq.gz \
     results/M1_4T1_replica1_Blood
--- docs/mixcr/guides/generic-multiplex-bcr/scripts/030-upstream-parallel.sh
+++ docs/mixcr/guides/generic-multiplex-bcr/scripts/030-upstream-parallel.sh
@@ -5,12 +5,12 @@ mkdir -p results/
 ls raw/*_1* | \
 	parallel -j 4 \
 	'mixcr analyze bcr-amplicon -f \
-	+species mmu \
-	+rna \
-	+rigidLeftAlignmentBoundary \
-	+rigidRightAlignmentBoundary \
-	+tagPattern "^N{"4"}ccctcctttaattcccN{"22"}(R2:*)\^N{"4"}gaggagagagagagagN{"26"}(R1:*)" \
-	+assembleClonotypesBy {"CDR1Begin:FR4End"} \
+	--species mmu \
+	--rna \
+	--rigid-left-alignment-boundary \
+	--rigid-right-alignment-boundary \
+	--tagPattern "^N{"4"}ccctcctttaattccc{"21"}(R1:*)\^N{"4"}gaggagagagagagagN{"26"}(R2:*)" \
+	--assemble-clonotypes-by {CDR1Begin:FR4End} \
 	{} \
 	{=s:R1:R2:=} \
 	{=s:.*/:results/:;s:_R.*::=}'
